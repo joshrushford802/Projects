a1960e3b76099a3c87488fa6c844a41968700f89
Joshua Rushford <109701246+joshrushford802@users.noreply.github.com>
2023-01-15 13:33:29 -0500

    Delete app directory

--- app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import EmailList, NewsArticle, InvasiveReport, Donation
-
-admin.site.register(EmailList)
-admin.site.register(NewsArticle)
-admin.site.register(InvasiveReport)
-admin.site.register(Donation)
